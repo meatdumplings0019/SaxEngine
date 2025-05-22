ac484392280b5687d487c65d9a0d3fae83fa4669
YourMD <meatdumplings@163.com>
2025-05-22 21:42:53 +0800

    Add fileManager

---
+++ src/File/__init__.py
@@ -0,0 +1,23 @@
+﻿from typing import Any
+
+from src.Libs.message import Message
+
+
+class FileManager:
+    def __init__(self, path: str):
+        self.path = path
+
+    def open(self) -> Message[Any]:
+        try:
+            with open(self.path, "r", encoding="utf-8") as file:
+                return Message(file.read())
+        except Exception as e:
+            return Message(None, e)
+
+    def write(self, content) -> Message[bool]:
+        try:
+            with open(self.path, "w", encoding="utf-8") as file:
+                file.write(content)
+                return Message(True)
+        except Exception as e:
+            return Message(False, e)
